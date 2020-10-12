61330bdd5e5a84316d2ff578ba534ad4feb0d14c
ceyhuncengiz <68386773+ceyhuncengiz@users.noreply.github.com>
2020-10-12 06:34:53 +0300

    Updates

---
+++ precache-manifest.b2456f67e5ca1f6ecbdb1a4df596f265.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0467970a4860eea665a741c865ae4762",
+    "url": "/movies-index-app/index.html"
+  },
+  {
+    "revision": "ddef92250ffbed34d864",
+    "url": "/movies-index-app/static/css/main.1fd04c07.chunk.css"
+  },
+  {
+    "revision": "782a391c04459a236424",
+    "url": "/movies-index-app/static/js/2.fa4f2714.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movies-index-app/static/js/2.fa4f2714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddef92250ffbed34d864",
+    "url": "/movies-index-app/static/js/main.7757713b.chunk.js"
+  },
+  {
+    "revision": "a6e32d0fb9d37b378ce8",
+    "url": "/movies-index-app/static/js/runtime-main.da24a72b.js"
+  }
+]);
